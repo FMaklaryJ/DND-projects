a271e3fd46dd8bcb3a1910dbc197e942eabc5403
FMaklaryJ <42065595+FMaklaryJ@users.noreply.github.com>
2019-02-21 14:48:00 +0100

    Fixed a minor miscalculation.

--- Probabilities.py
+++ Probabilities.py
@@ -82,6 +82,12 @@ def spread(distribution,average): #Returns the standard deviation
     spr=(np.sum(tau))/total;
     spr=math.sqrt(spr)
     return spr;
+def partdist(dist,HP):
+    s=dist.copy()
+    if dist.size>=HP:
+        s=s[0:HP]
+    s=s/sum(s)
+    return s
 
 def survprops(distribution,Edistribution,rounds,HP,EHP):
     #Returns probability for victory, assuming constant dmg distribution
@@ -105,11 +111,12 @@ def survprops(distribution,Edistribution,rounds,HP,EHP):
     H[2,1]=1-Esurv;
     H[1,1]=Esurv;
     P0=np.matmul(H,M,out=None);
+    
     for s in range(rounds-1):
         surv=1.0;
         Esurv=1.0;
-        d=matfold(d,distribution);
-        Ed=matfold(Ed,Edistribution);
+        d=matfold(partdist(d,EHP),distribution);
+        Ed=matfold(partdist(Ed,HP),Edistribution);
         if d.size>=HP:
             surv=0.0;
             for i in range(HP-1):
@@ -135,20 +142,7 @@ def survprops(distribution,Edistribution,rounds,HP,EHP):
         
 
 
-#atk=#input("Input attack score: ");
-#atk=int(atk);
-#AC=input("Input enemy AC: ");
-#AC=int(AC);
-#sides=input("Input number of sides on dmg dice: ");
-#sides=int(sides);
-#N=input("Input number of this dmg dice: ");
-#N=int(N);
-#flat=input("Input flat damage (no dice): ");
-#flat=int(flat);
-#natk=input("How many attacks do you do with these? (at least 1)");
-#natk=int(natk);
-#atk2=input("Do you have a second type of dmg dice? (1 for yes, 0 for no)");
-#atk2=int(atk2);
+
 
 
 #User's parameters (this guy goes first)
@@ -176,35 +170,13 @@ Enatk=4;
 EHP=84;
 
 
-#------------Ignore this, this is for other cases than the current one---     
-if atk2==1:
-    sides2=input("Input number of sides on dmg dice: ");
-    sides2=int(sides2);
-    N2=input("Input number of this dmg dice: ");
-    N2=int(N2);
-     
-#sneak=input("Do you have sneak attack, or similar? (1 for yes, 0 for no)");
-#sneak=int(sneak);
-sneak=0;
-if sneak==1:
-    sidesneak=input("How many sides on extra dice? ");
-    sidesneak=int(sidesneak);
-    Nsneak=input("How many extra dice? ");
-    Nsneak=int(Nsneak);
-#-----------------------------------------------------------------------
 
 
 Ed=Ndice(EN,Esides);
 d=Ndice(N,sides);
 
 
-#----------Ignore this too---------------------------------------------
-if atk2==1:
-    if sides2>=1:
-        if N2>=1:
-            d2=Ndice(N2,sides);
-            d=matfold(d,d2);
-#--------------------------------------------------------------------            
+        
 Edistribution=activate(Eatk,EAC,Ed,Eflat);
 distribution=activate(atk,AC,d,flat);
 
@@ -217,20 +189,6 @@ if Enatk>=2:
     Edist2=Edistribution;
     for s in range(Enatk-1):
         Edistribution=matfold(Edist2,Edistribution);
-
-#----------------This isn't used at the moment---------------------
-if sneak==1:
-    sneakdice=Ndice(Nsneak,sidesneak);
-    distribution=sneak(distribution,sneakdice);
-
-
-
-
-
-damage=np.array([0.0]*distribution.size);
-for s in range(distribution.size):
-    damage[s]=s;
-
 #-----------------------------------------------------------------
 
 z=np.array([0.0]*10); #Turn-counter
